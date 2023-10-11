91a7598f5fa2d1adc0c5310763d96394e02f775c
khadka-srj <83262160+khadka-srj@users.noreply.github.com>
2023-10-11 15:01:35 +0545

    added 404 custom image and css

--- src/app/components/page-not-found-component/page-not-found-component.component.ts
+++ src/app/components/page-not-found-component/page-not-found-component.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found-component',
   templateUrl: './page-not-found-component.component.html',
-  styleUrls: ['./page-not-found-component.component.scss']
+  styleUrls: ['./page-not-found-component.component.scss'],
 })
 export class PageNotFoundComponentComponent {
-
+  constructor(private router: Router) {}
+  redirectHome() {
+    this.router.navigate(['/']);
+  }
 }
