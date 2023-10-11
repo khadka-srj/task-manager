0e9564e952a64899e4401b6a64f0d3171f6682ff
khadka-srj <83262160+khadka-srj@users.noreply.github.com>
2023-10-11 16:58:53 +0545

    added a new message in home component if there are no tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { AddTaskComponent } from './components/add-task/add-task.component';
 import { Task } from 'src/Task';
 import { UserServiceService } from './services/user.service';
 import { Router } from '@angular/router';
@@ -15,20 +13,13 @@ export class AppComponent implements OnInit {
   // yo chai if yo component refresh bhayo bhane ya bata data populate huncha.
   currentUser = JSON.parse(localStorage.getItem('currentUser')!);
   title = 'testapp';
-  constructor(
-    private _dialog: MatDialog,
-    private userService: UserServiceService,
-    private router: Router
-  ) {
+  constructor(private userService: UserServiceService, private router: Router) {
     this.userService.getCurrentUser().subscribe((value) => {
       this.currentUser = value;
     });
   }
   ngOnInit(): void {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser')!);
-  }
-  openAddEditDialog() {
-    this._dialog.open(AddTaskComponent);
   }
 
   logout() {
